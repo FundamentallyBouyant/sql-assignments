cc53192914807bb382144e4c0e03c79e3cffef69
Jay Dadhania <jcdadhania@gmail.com>
2017-09-13 14:49:12 +0530

    changes

--- imdb/imdb.sql
+++ imdb/imdb.sql
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS imdb.tvseries(
     name VARCHAR(100) NOT NULL,
     release_date DATE,
     episodes INT UNSIGNED,
-    PRIMARY KEY(movie_id)
+    PRIMARY KEY(tvseries_id)
     );
 
 CREATE TABLE IF NOT EXISTS imdb.movie_actor(
@@ -37,8 +37,8 @@ CREATE TABLE IF NOT EXISTS imdb.movie_actor(
 CREATE TABLE IF NOT EXISTS imdb.tv_actor(
 	tvseries_id INT UNSIGNED NOT NULL,
     actor_id INT UNSIGNED NOT NULL,
-    CONSTRAINT fk_movie_actor_movie_id
-    FOREIGN KEY(movie_id) REFERENCES movies(movie_id),
-    CONSTRAINT fk_movie_actor_actor_id
+    CONSTRAINT fk_tv_actor_movie_id
+    FOREIGN KEY(tvseries_id) REFERENCES tvseries(tvseries_id),
+    CONSTRAINT fk_tv_actor_actor_id
     FOREIGN KEY(actor_id) REFERENCES actors(id)
     );
